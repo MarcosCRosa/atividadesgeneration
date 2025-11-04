99db71d73fb6b0cb788fe6869652133872a0485e
MarcosCRosa <marcosluiz4012@gmail.com>
2025-11-04 20:18:27 -0300

    refazendo o primeiro exercicio de set agora de uma maneira que eu entenda o que to fazendo

--- exerciciosArrayeLet/primeiraAtividadeSet.ts
+++ exerciciosArrayeLet/primeiraAtividadeSet.ts
@@ -1,26 +1,38 @@
-import readline from 'readline-sync';
+ import readline, { questionInt } from 'readline-sync';
 
-const numeros = new Set<number>();
-const todasEntradas: number[] = [];
+ const numeros:Set<number> = new Set<number>();
+ let numeroDigitado;
+
+// const todasEntradas: number[] = [];
 
 
-while (todasEntradas.length < 10) {
-    const numeroDigitado = readline.questionInt("Digite um numero: ");
-    todasEntradas.push(numeroDigitado);
-    numeros.add(numeroDigitado);
+// while (todasEntradas.length < 10) {
+//     const numeroDigitado = readline.questionInt("Digite um numero: ");
+//     todasEntradas.push(numeroDigitado);
+//     numeros.add(numeroDigitado);
+// }
+
+// console.log("Numeros Digitados:", ...numeros);
+
+// const ordenados = [...numeros].sort((a, b) => a - b);
+// console.log("Numeros Em Ordem Crescente:", ...ordenados);
+
+// const repetidos = [...numeros].filter(valor => 
+//     todasEntradas.filter(n => n === valor).length > 1
+// );
+
+// if (repetidos.length > 0) {
+//     console.log("Numeros Repetidos:", ...repetidos);
+// } else {
+//     console.log("Nenhum numero repetido.");
+// }
+for(let numero=0; numero<10; numero++){
+numeroDigitado = questionInt("Digite um numero:");
+numeros.add(numeroDigitado);
+
+
 }
-
-console.log("Numeros Digitados:", ...numeros);
-
-const ordenados = [...numeros].sort((a, b) => a - b);
-console.log("Numeros Em Ordem Crescente:", ...ordenados);
-
-const repetidos = [...numeros].filter(valor => 
-    todasEntradas.filter(n => n === valor).length > 1
-);
-
-if (repetidos.length > 0) {
-    console.log("Numeros Repetidos:", ...repetidos);
-} else {
-    console.log("Nenhum numero repetido.");
-}
+const numerosCrescentes:Array<number>= Array.from(numeros);
+numerosCrescentes.sort();
+console.log(`Ordem Crescente:${numerosCrescentes.sort((a,b)=>a-b)}`);
+console.log("Numeros Digitados:",...numeros);
